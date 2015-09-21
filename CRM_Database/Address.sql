d3cc029b35bda21f42b7d6bb990ad7950351f055
djustice <dave.justice@mykolab.com>
2015-09-20 18:16:35 -0600

    Initial commit to test

---
+++ CRM_Database/Address.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[AddressId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [AddressTypeId] INT NOT NULL, 
+    [AddressLine1] NVARCHAR(60) NOT NULL, 
+    [AddressLine2] NVARCHAR(60) NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [StateProvinceId] INT NOT NULL, 
+    [PostalCode] NVARCHAR(15) NOT NULL, 
+    [DateUpdated] DATETIME NOT NULL, 
+    [UserUpdated] INT NOT NULL, 
+    [IsActive] BIT NOT NULL DEFAULT 0x1
+)
